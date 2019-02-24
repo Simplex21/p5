2d3965c6d0d72651c4a343b45b99cd248f7302f0
Simplex21 <21simplex02@gmail.com>
2019-02-24 21:45:58 +0100

    coordinateSystem()

--- sin_chart/sketch.js
+++ sin_chart/sketch.js
@@ -4,31 +4,31 @@ var inc;
 
 function setup() {
 	createCanvas(600, 600);
-	inc = TWO_PI/25.0;
+	inc = TWO_PI / 25.0;
 
 	coordinateSystem();
 }
 
 function coordinateSystem() {
-	line(0, height/2, width, height/2);
-	line(width/2, height, width/2, 0);
-	
+	line(0, height / 2, width, height / 2);
+	line(width / 2, height, width / 2, 0);
+
 	// save the sin instances
-	for (var i = 0; i < width; i+=distance) {
-	  fill(0);
-	  var multiplicator = 20.0;
-	  ellipse(i, height/2+(sin(angle)*multiplicator), 5, 5);
-	  
-	  line(i, height/2+(sin(angle)*multiplicator), i + distance, height/2+(sin(angle + inc)*multiplicator));
-	  angle = angle + inc;
+	for (var i = 0; i < width; i += distance) {
+		fill(0);
+		var multiplicator = 20.0;
+		ellipse(i, height / 2 + (sin(angle) * multiplicator), 5, 5);
+
+		line(i, height / 2 + (sin(angle) * multiplicator), i + distance, height / 2 + (sin(angle + inc) * multiplicator));
+		angle = angle + inc;
 	}
-	
+
 	// x-Achse beschriften
-	for (var i = 0; i < width; i+=distance) {
-	  line(i, height/2 + 5, i, height/2 - 5);
+	for (var i = 0; i < width; i += distance) {
+		line(i, height / 2 + 5, i, height / 2 - 5);
 	}
 	// y-Achse beschriften
-	for (var i = height; i > 0; i-=distance) {
-	  line(height/2 + 5, i, width/2 - 5, i);
+	for (var i = height; i > 0; i -= distance) {
+		line(height / 2 + 5, i, width / 2 - 5, i);
 	}
-  }
+}
